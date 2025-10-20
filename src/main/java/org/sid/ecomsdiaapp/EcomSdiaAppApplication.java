01fb9b1c0ef53df83ee1f1a11898ae70477f7fc9
khemisse-radwane <khemisse.radwane@gmail.com>
2025-10-20 22:44:49 +0100

    project cration

---
+++ src/main/java/org/sid/ecomsdiaapp/EcomSdiaAppApplication.java
@@ -0,0 +1,13 @@
+package org.sid.ecomsdiaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomSdiaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomSdiaAppApplication.class, args);
+    }
+
+}
